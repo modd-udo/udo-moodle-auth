8d15457942e060744cec2923f7d682db49f0e9d8
Chris Seufert <chris@seufert.id.au>
2017-02-23 19:32:00 +1100

    Added text for settings

--- lang/en/auth_udo.php
+++ lang/en/auth_udo.php
@@ -10,3 +10,9 @@
 
 $string['auth_udodescription'] = 'Allows Udo Website Users to login to Moodle, without creating accounts on Moodle.';
 $string['pluginname'] = 'Udo Website Connector';
+$string['auth_udo_hmac_secret'] = 'HMAC Secret Key';
+$string['auth_udo_hmac_secret_description'] = 'This is the secret key that is used to validate logins from your udo website';
+$string['auth_udo_login_url'] = 'Login URL';
+$string['auth_udo_login_url_description'] = 'URL to redirect to rather than showing moodle login form';
+$string['auth_udo_changepass_url'] = 'Change Password URL';
+$string['auth_udo_changepass_url_description'] = 'URL to send user to, when they want to change their password';
